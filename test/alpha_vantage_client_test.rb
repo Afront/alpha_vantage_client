aaec18af51834e7e6c72269d942075b7036445c9
Afront <3943720+Afront@users.noreply.github.com>
2020-01-17 23:38:35 +0900

    Add test for printing json

--- test/alpha_vantage_client_test.rb
+++ test/alpha_vantage_client_test.rb
@@ -4,4 +4,8 @@ class AlphaVantageClientTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::AlphaVantageClient::VERSION
   end
+
+  def test_for_printing_json
+    API::test(function: "CURRENCY_EXCHANGE_RATE", from_currency: "USD", to_currency: "TWD")
+  end
 end
