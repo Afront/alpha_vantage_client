4a6a03a34703ef537a45eb6b3a0230fe009dcb3b
Afront <3943720+Afront@users.noreply.github.com>
2020-01-17 23:57:56 +0900

    Add function_type hash for test function

--- lib/alpha_vantage_client/api.rb
+++ lib/alpha_vantage_client/api.rb
@@ -4,10 +4,27 @@ require 'JSON'
 module API
   raise IOError, 'ALPHA_VANTAGE_API_KEY is not set as an environmental variable' if ENV['ALPHA_VANTAGE_API_KEY'].nil?
   API_KEY = ENV['ALPHA_VANTAGE_API_KEY'].to_s
+  
+  @function_type = {
+        "TIME_SERIES_INTRADAY"=> :stock_market_function,
+        "TIME_SERIES_DAILY"=> :stock_market_function,
+        "TIME_SERIES_DAILY_ADJUSTED"=> :stock_market_function,
+        "TIME_SERIES_WEEKLY"=> :stock_market_function,
+        "TIME_SERIES_WEEKLY_ADJUSTED"=> :stock_market_function,
+        "TIME_SERIES_MONTHLY"=> :stock_market_function,
+        "TIME_SERIES_MONTHLY_ADJUSTED"=> :stock_market_function,
+        "QUOTE_ENDPOINT"=> :stock_market_function,
+        "SEARCH_ENDPOINT"=> :stock_market_function,
+        "CURRENCY_EXCHANGE_RATE"=> :forex_function,
+        "FX_INTRADAY"=> :forex_function,
+        "FX_DAILY"=> :forex_function,
+        "FX_WEEKLY"=> :forex_function,
+        "FX_MONTHLY"=> :forex_function,     
+  }
+
   CallStruct = Struct.new(:function, :from_currency, :to_currency, :from_symbol, :to_symbol, :symbol, :interval, :outputsize, :datatype, :keywords) do
     def get_url #previously validate_data, get_valid_arr, generate_url
-      parameters = {
-        #stock market functions
+      @stock_market_parameters = {
         "TIME_SERIES_INTRADAY"=> {:required => [function, symbol, interval], :optional => [outputsize, datatype]}, 
         "TIME_SERIES_DAILY"=> {:required => [function, symbol], :optional => [outputsize, datatype]},  
         "TIME_SERIES_DAILY_ADJUSTED"=> {:required => [function, symbol], :optional => [outputsize, datatype]},
@@ -16,15 +33,17 @@ module API
         "TIME_SERIES_MONTHLY"=> {:required => [function, symbol], :optional => [datatype]},
         "TIME_SERIES_MONTHLY_ADJUSTED"=> {:required => [function, symbol], :optional => [datatype]},
         "QUOTE_ENDPOINT"=> {:required => [function, symbol], :optional => [datatype]},
-        "SEARCH_ENDPOINT"=> {:required => [function, keywords], :optional => [datatype]},
+        "SEARCH_ENDPOINT"=> {:required => [function, keywords], :optional => [datatype]}
+      }
 
-        #forex functions
-        "CURRENCY_EXCHANGE_RATE"=> {:required => [function, from_currency, to_currency], :optional => []}, 
-        "FX_INTRADAY"=> {:required => [function, from_symbol, to_symbol, interval], :optional => [outputsize, datatype]},  
-        "FX_DAILY"=> {:required => [function, from_symbol, to_symbol], :optional => [outputsize, datatype]},
-        "FX_WEEKLY"=> {:required => [function, from_symbol, to_symbol], :optional => [datatype]},
-        "FX_MONTHLY"=> {:required => [function, from_symbol, to_symbol], :optional => [datatype]},
-        }
+      @forex_parameters = {
+            "CURRENCY_EXCHANGE_RATE"=> {:required => [function, from_currency, to_currency], :optional => []}, 
+            "FX_INTRADAY"=> {:required => [function, from_symbol, to_symbol, interval], :optional => [outputsize, datatype]},  
+            "FX_DAILY"=> {:required => [function, from_symbol, to_symbol], :optional => [outputsize, datatype]},
+            "FX_WEEKLY"=> {:required => [function, from_symbol, to_symbol], :optional => [datatype]},
+            "FX_MONTHLY"=> {:required => [function, from_symbol, to_symbol], :optional => [datatype]}
+      }
+      parameters = @stock_market_parameters + @forex_parameters
 
       required_parameters = parameters[function][:required]
       optional_parameters = parameters[function][:optional]
@@ -54,8 +73,15 @@ module API
   def print_json json
   end
 
-  def test(function:, symbol: false, interval: false, outputsize: false, datatype: false, keywords: false)
-    call = CallStruct.new(function, symbol, interval, outputsize, datatype, keywords)
+  def test(function:, from_currency: false, to_currency: false, from_symbol: false, to_symbol: false, symbol: false, interval: false, outputsize: false, datatype: false, keywords: false)
+    call = case @function_type[function]
+    when :stock_market_function
+      CallStruct.new(function, symbol, interval, outputsize, datatype, keywords)
+    when :forex_function      
+      CallStruct.new(function, from_currency, to_currency, from_symbol, to_symbol, interval, outputsize, datatype)
+    else 
+      raise NameError, 'Invalid function: #{function}'
+    end
     print_json(get_json(call))
   end
 end
