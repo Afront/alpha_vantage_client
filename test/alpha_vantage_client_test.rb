6d4e5d4ee6f884c0ddab06fd475dd5beb76e0809
Afront <3943720+Afront@users.noreply.github.com>
2020-01-18 20:36:46 +0900

    Replace UT with Spec

--- test/alpha_vantage_client_test.rb
+++ test/alpha_vantage_client_test.rb
@@ -5,10 +5,38 @@ class AlphaVantageClientTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::AlphaVantageClient::VERSION
   end
+end
 
-  def test_for_printing_json
-    API::test(function: "CURRENCY_EXCHANGE_RATE", from_currency: "USD", to_currency: "BTC")
-    API::test(function: "FX_INTRADAY", from_symbol: "USD", to_symbol: "TWD", interval: "5min")
-    API::test(function: "FX_DAILY", from_symbol: "USD", to_symbol: "ZWL", outputsize: "full")
+describe API do
+  describe 'when forex functions are used' do
+    describe 'when "CURRENCY_EXCHANGE_RATE" function is used' do
+      before do 
+        @supposed_hash_object = API::get_directly(function: "CURRENCY_EXCHANGE_RATE", from_currency: "USD", to_currency: "BTC")
+      end 
+
+      it 'must return a Hash object' do
+        _(@supposed_hash_object).must_be_kind_of Hash
+      end
+    end
+
+    describe 'when the "FX_INTRADAY" function is used' do
+      before do 
+        @supposed_hash_object = API::get_directly(function: "FX_INTRADAY", from_symbol: "USD", to_symbol: "TWD", interval: "5min")
+      end
+
+      it 'must return a Hash object' do 
+      _(@supposed_hash_object).must_be_kind_of Hash
+      end
+    end
+
+    describe 'when the "FX_DAILY" function is used' do
+      before do 
+        @supposed_hash_object = API::get_directly(function: "FX_DAILY", from_symbol: "AUD", to_symbol: "USD", outputsize: "full")
+      end
+
+      it 'must return a Hash object' do 
+      _(@supposed_hash_object).must_be_kind_of Hash
+      end
+    end
   end
 end
