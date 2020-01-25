83f75263b38a67c4b325ae62c3ce17b5cfd2980e
Afront <3943720+Afront@users.noreply.github.com>
2020-01-25 23:15:45 +0900

    Refactor api functions by more than 50 lines

--- lib/alpha_vantage_client/api.rb
+++ lib/alpha_vantage_client/api.rb
@@ -1,79 +1,34 @@
 require 'faraday'
 require 'json'
+require 'toml-rb'
 
 module API
   raise IOError, 'ALPHA_VANTAGE_API_KEY is not set as an environmental variable' if ENV['ALPHA_VANTAGE_API_KEY'].nil?
   API_KEY = ENV['ALPHA_VANTAGE_API_KEY'].to_s
   
-  @function_type = {
-        "TIME_SERIES_INTRADAY"=> :stock_market_function,
-        "TIME_SERIES_DAILY"=> :stock_market_function,
-        "TIME_SERIES_DAILY_ADJUSTED"=> :stock_market_function,
-        "TIME_SERIES_WEEKLY"=> :stock_market_function,
-        "TIME_SERIES_WEEKLY_ADJUSTED"=> :stock_market_function,
-        "TIME_SERIES_MONTHLY"=> :stock_market_function,
-        "TIME_SERIES_MONTHLY_ADJUSTED"=> :stock_market_function,
-        "QUOTE_ENDPOINT"=> :stock_market_function,
-        "SEARCH_ENDPOINT"=> :stock_market_function,
-        "CURRENCY_EXCHANGE_RATE"=> :forex_function, #also a crypto function, but DRY...will change this when refactoring
-        "FX_INTRADAY"=> :forex_function,
-        "FX_DAILY"=> :forex_function,
-        "FX_WEEKLY"=> :forex_function,
-        "FX_MONTHLY"=> :forex_function,     
-        "DIGITAL_CURRENCY_DAILY"=> :crypto_function,
-        "DIGITAL_CURRENCY_WEEKLY"=> :crypto_function,
-        "DIGITAL_CURRENCY_MONTHLY"=> :crypto_function,
-        "SECTOR" => :sector_function, 
-  }
+  @@functions_info = TomlRB.load_file('lib/alpha_vantage_client/api.toml')
+
+  def self.functions_info #might remove this to make it private
+    @@functions_info 
+  end
+
 
   CallStruct = Struct.new(:function, :from_currency, :to_currency, :from_symbol, :to_symbol, :symbol, :market, :interval, :outputsize, :datatype, :keywords, keyword_init: true) do
     def get_url #previously validate_data, get_valid_arr, generate_url
-      @stock_market_parameters = {
-        "TIME_SERIES_INTRADAY"=> {:required => [function, symbol, interval], :optional => [outputsize, datatype]}, 
-        "TIME_SERIES_DAILY"=> {:required => [function, symbol], :optional => [outputsize, datatype]},  
-        "TIME_SERIES_DAILY_ADJUSTED"=> {:required => [function, symbol], :optional => [outputsize, datatype]},
-        "TIME_SERIES_WEEKLY"=> {:required => [function, symbol], :optional => [datatype]},
-        "TIME_SERIES_WEEKLY_ADJUSTED"=> {:required => [function, symbol], :optional => [datatype]},
-        "TIME_SERIES_MONTHLY"=> {:required => [function, symbol], :optional => [datatype]},
-        "TIME_SERIES_MONTHLY_ADJUSTED"=> {:required => [function, symbol], :optional => [datatype]},
-        "QUOTE_ENDPOINT"=> {:required => [function, symbol], :optional => [datatype]},
-        "SEARCH_ENDPOINT"=> {:required => [function, keywords], :optional => [datatype]}
-      }
-
-      @forex_parameters = {
-            "CURRENCY_EXCHANGE_RATE"=> {:required => [function, from_currency, to_currency], :optional => []}, 
-            "FX_INTRADAY"=> {:required => [function, from_symbol, to_symbol, interval], :optional => [outputsize, datatype]},  
-            "FX_DAILY"=> {:required => [function, from_symbol, to_symbol], :optional => [outputsize, datatype]},
-            "FX_WEEKLY"=> {:required => [function, from_symbol, to_symbol], :optional => [datatype]},
-            "FX_MONTHLY"=> {:required => [function, from_symbol, to_symbol], :optional => [datatype]}
-      }
-
-      @crypto_parameters = {
-            "DIGITAL_CURRENCY_INTRADAY"=> {:required => [function, symbol, market, interval], :optional => [datatype]},  
-            "DIGITAL_CURRENCY_DAILY"=> {:required => [function, symbol, market], :optional => [datatype]},
-            "DIGITAL_CURRENCY_WEEKLY"=> {:required => [function, symbol, market], :optional => [datatype]},
-            "DIGITAL_CURRENCY_MONTHLY"=> {:required => [function, symbol, market], :optional => [datatype]}
-      }
-
-      @sector_parameter = {
-            "SECTOR"=> {:required => [function], :optional => []}
-      }
-
-
-      parameters = @stock_market_parameters.merge(@forex_parameters).merge(@crypto_parameters).merge(@sector_parameter)
-
-      required_parameters = parameters[function][:required]
-      optional_parameters = parameters[function][:optional]
-
       arr_err = []
       arr_valid = []
+
+      required_parameters = @@functions_info[function]["required"]
+      optional_parameters = @@functions_info[function]["optional"]
+
       self.each_pair do |parameter, value|
-        arr_err << "#{parameter} is not set" if !value && (required_parameters.include? value) #need to fix name choices soon
-        arr_err << "#{parameter} should not be set for #{function}" unless (required_parameters+optional_parameters).include?(value) || !value
+        parameter_as_string = parameter.to_s
+        arr_err << "#{parameter} is not set" if !value && (required_parameters.include? parameter_as_string)
+        arr_err << "#{parameter} should not be set for #{function}" unless (required_parameters+optional_parameters).include?(parameter_as_string) || !value
         arr_valid << "#{parameter}=#{value}" if value
       end
 
-      raise ArgumentError, ' ' + arr_err.join("\n\t\t") + "\n" unless arr_err.empty? #fix newline
+      raise ArgumentError, ' ' + arr_err.join("\n\t\t") + "\n" unless arr_err.empty?
 
       'https://www.alphavantage.co/query?' + arr_valid.join('&') + '&apikey=' + API_KEY
     end
@@ -99,19 +54,8 @@ module API
   end
 
   def get_directly(function:, from_currency: false, to_currency: false, from_symbol: false, to_symbol: false, symbol: false, market: false, interval: false, outputsize: false, datatype: false, keywords: false)
-    call = case @function_type[function]
-    when :stock_market_function
-      CallStruct.new(function: function, symbol: symbol, interval: interval, outputsize:outputsize, datatype: datatype, keywords: keywords)
-    when :forex_function      
-      CallStruct.new(function: function, from_currency: from_currency, to_currency: to_currency, from_symbol: from_symbol, to_symbol: to_symbol, interval: interval, outputsize: outputsize, datatype: datatype)
-    when :crypto_function
-      CallStruct.new(function: function, symbol: symbol, market: market, datatype: datatype)  
-    when :sector_function
-      CallStruct.new(function: function)  
-    else 
-      raise NameError, "Invalid function: #{function}"
-    end
-    get_json call
+    raise NameError, "Invalid function: #{function}" unless @@functions_info[function] 
+    get_json CallStruct.new(function: function, from_currency: from_currency, to_currency: to_currency, from_symbol: from_symbol, to_symbol: to_symbol, symbol: symbol, market: market, interval: interval, outputsize: outputsize, datatype: datatype, keywords: keywords)
   end
 
   def print_directly(function:, from_currency: false, to_currency: false, from_symbol: false, to_symbol: false, symbol: false, market: false, interval: false, outputsize: false, datatype: false, keywords: false)
