5b9777baaf9b0b39b682a1ee61cb508b8a5bd3d1
Afront <3943720+Afront@users.noreply.github.com>
2020-01-31 23:57:50 +0900

    Add documentation and more module methods

--- lib/alpha_vantage_wrapper/api.rb
+++ lib/alpha_vantage_wrapper/api.rb
@@ -4,8 +4,12 @@ require 'faraday'
 require 'json'
 require 'toml-rb'
 
-# (TODO:) Add documentation here
+#
+# This module contains the methods to access the api.
+# @author [Afront]
+# 
 module API
+
   # A class for functions_info; originally a struct, but changed it to class
   class FunctionsInfo
     def initialize
@@ -63,6 +67,41 @@ module API
     def info(function)
       @info[function]
     end
+
+    def generate_url(hash, arr_err = [], hash_str = '')
+      hash.each_pair do |parameter, value|
+        # assumes all values are not nil
+        # add `raise ''... if hash contains nil as a value` if needed
+        hash_str += "#{parameter}=#{value}"
+        if parameter?(hash[:function], parameter)
+          arr_err << "#{parameter} should not be set for #{hash[:function]}"
+        elsif required_parameter?(hash[:function], parameter)
+          arr_err << "#{parameter} is not set"
+        end
+      end
+
+      raise ArgumentError, "#{arr_err.join("\n\t\t")}\n" unless arr_err.empty?
+
+      "https://www.alphavantage.co/query?#{hash_str}&apikey=#{hash[:apikey]}"
+    end
+
+    def get_json(call_hash)
+      load_key call_hash[:apikey]
+
+      json_result = Faraday.get generate_url(call_hash)
+      JSON.parse json_result.body
+    end
+
+    def print_json(json, depth = 0)
+      json.each do |key, value|
+        if value.is_a? Hash
+          puts "#{key}:"
+          print_json value, depth + 1
+        else
+          puts "#{"\t" * depth}#{key}: #{value}"
+        end
+      end
+    end
   end
 
   @functions_info = FunctionsInfo.new
@@ -73,63 +112,81 @@ module API
     @functions_info.load type
   end
 
-  # This module contains specific methods
-  # and a general method for cryptocurrencies
+  # This module handles errors concerning the API
+  module ErrorHandler
+    def missing_a
+      
+    end
+  end
+
+  # This module contains specific methods for cryptocurrencies functions
   module Crypto
   end
 
-  # This module contains specific methods
-  # and a general method for forex (fx)
+  # This module contains specific methods for forex functions
   module Forex
+
+    def currency_exchange_rate(from_currency:, to_currency:, apikey: false)
+    	query_string{'from_currency' => from_currency,
+    	 'to_currency'   => to_currency,
+    	 'apikey' 			 => apikey}.map do |parameter, argument|
+    	 		"#{parameter}=#{argument}"
+    	 }.join('&')
+
+	    get_json "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&#{query_string}"
+    end
+
+    def intraday(from_symbol:, to_symbol:, interval, outputsize: false, datatype: false, apikey: false)
+      
+    end
+
+
+    def daily(from_symbol: false, to_symbol: false, apikey: false)
+      
+    end
+
   end
 
-  # This module contains a method for sector
+  # This module contains a method for the sector function
   module Sector
+
   end
 
-  # This module contains specific methods
-  # and a general method for stocks
+  # This module contains specific methods for stocks functions
   module Stocks
   end
 
-  # This module contains specific methods
-  # and a general method for stocks
+  # This module contains specific methods for technical indicators functions
   module TechIndicators
   end
 
-  def generate_url(hash, arr_err = [], valid_string = '')
-    hash.each_pair do |parameter, value|
-      # assumes all values are not nil
-      # add `raise ''... if hash contains nil as a value` if needed
-      valid_string += "#{parameter}=#{value}"
-      if @functions_info.parameter?(hash[:function], parameter)
-        arr_err << "#{parameter} should not be set for #{hash[:function]}"
-      elsif @functions_info.required_parameter?(hash[:function], parameter)
-        arr_err << "#{parameter} is not set"
-      end
-    end
-
-    raise ArgumentError, "#{arr_err.join("\n\t\t")}\n" unless arr_err.empty?
-
-    "https://www.alphavantage.co/query?#{valid_string}&apikey=#{hash[:apikey]}"
+  def get_json(url)
+    json_result = Faraday.get url
+    JSON.parse json_result.body  
   end
 
-  def get_json(call_hash)
-    @functions_info.load_key call_hash[:apikey]
-
-    json_result = Faraday.get generate_url(call_hash)
-    JSON.parse json_result.body
+  # Gets the sector information using the API
+  # @note The API key is optional as the demo key can be used AFAIK.
+  # @param apikey [apikey] The Alpha Vantage API key
+  # @return [Hash] The hash received from the API response
+  def get_sector(apikey: false)
+    apikey ||= demo
+    get_json "https://www.alphavantage.co/query?SECTOR&apikey=#{apikey}"
   end
 
-  def print_json(json, depth = 0)
-    json.each do |key, value|
-      if value.is_a? Hash
-        puts "#{key}:"
-        print_json value, depth + 1
-      else
-        puts "#{"\t" * depth}#{key}: #{value}"
-      end
-    end
+ 
+	# Get stock information using the api
+	# @param **stock_parameters [Hash] Parameters for the desired function
+  # @return [Hash] The hash received from the API response
+  def get_stocks(**stock_parameters)
+
+  end
+
+	# Get technical indicator information using the api
+	# @param **tech_indicator_parameters [Hash] Parameters for the function
+  # @return [Hash] The hash received from the API response
+  def get_tech_indicators(**tech_indicator_parameters)
+    
   end
 
   def get_directly(**input_function_parameters)
@@ -139,10 +196,10 @@ module API
     @functions_info.load 'all' if @functions_info.not_loaded
     raise NameError, invalid_function_w unless @functions_info.include? function
 
-    get_json(input_function_parameters)
+    @functions_info.get_json input_function_parameters
   end
 
   def print_directly(**input_function_parameters)
-    print_json get_json(input_function_parameters)
+    @functions_info.print_json get_directly(input_function_parameters)
   end
 end
